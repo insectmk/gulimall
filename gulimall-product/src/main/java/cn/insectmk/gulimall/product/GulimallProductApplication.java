7692f643640df7400b8a4283862b0093a444d171
makuning <makun_ing@126.com>
2024-05-09 10:31:32 +0800

    完成商品微服务Mybatis-Plus配置进行测试

--- gulimall-product/src/main/java/cn/insectmk/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/cn/insectmk/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,10 @@
 package cn.insectmk.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cn.insectmk.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/cn/insectmk/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/cn/insectmk/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,24 @@
 package cn.insectmk.gulimall.product;
 
+import cn.insectmk.gulimall.product.entity.BrandEntity;
+import cn.insectmk.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
+    @Autowired
+    private BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+
+        brandEntity.setName("华为");
+
+        brandService.save(brandEntity);
+        System.out.println("保存成功");
     }
 
 }
