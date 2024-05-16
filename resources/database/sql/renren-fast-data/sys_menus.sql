49da9f1d7758fcd18792109bcced4df29610d342
makuning <makun_ing@126.com>
2024-05-16 17:19:33 +0800

    人人后台管理-增加菜单sql

--- resources/database/sql/renren-fast-data/sys_menus.sql
+++ resources/database/sql/renren-fast-data/sys_menus.sql
@@ -1,3 +1,23 @@
+/*
+SQLyog Ultimate v11.25 (64 bit)
+MySQL - 5.7.27 : Database - gulimall_admin
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mall_admin` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `mall_admin`;
+
+/*Table structure for table `sys_menu` */
+
 DROP TABLE IF EXISTS `sys_menu`;
 
 CREATE TABLE `sys_menu` (
