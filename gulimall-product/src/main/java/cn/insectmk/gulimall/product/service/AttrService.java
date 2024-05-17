f243b85233c37afe74e0bc3543e8c4b849337cf0
makuning <makun_ing@126.com>
2024-05-17 17:06:41 +0800

    商品服务-完成新增规格参数接口

--- gulimall-product/src/main/java/cn/insectmk/gulimall/product/service/AttrService.java
+++ gulimall-product/src/main/java/cn/insectmk/gulimall/product/service/AttrService.java
@@ -1,5 +1,6 @@
 package cn.insectmk.gulimall.product.service;
 
+import cn.insectmk.gulimall.product.vo.AttrVo;
 import com.baomidou.mybatisplus.extension.service.IService;
 import cn.insectmk.common.utils.PageUtils;
 import cn.insectmk.gulimall.product.entity.AttrEntity;
@@ -16,5 +17,7 @@ import java.util.Map;
 public interface AttrService extends IService<AttrEntity> {
 
     PageUtils queryPage(Map<String, Object> params);
+
+    void saveAttr(AttrVo attr);
 }
 
--- gulimall-product/src/main/java/cn/insectmk/gulimall/product/service/impl/AttrServiceImpl.java
+++ gulimall-product/src/main/java/cn/insectmk/gulimall/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package cn.insectmk.gulimall.product.service.impl;
 
+import cn.insectmk.gulimall.product.dao.AttrAttrgroupRelationDao;
+import cn.insectmk.gulimall.product.entity.AttrAttrgroupRelationEntity;
+import cn.insectmk.gulimall.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,10 +16,13 @@ import cn.insectmk.common.utils.Query;
 import cn.insectmk.gulimall.product.dao.AttrDao;
 import cn.insectmk.gulimall.product.entity.AttrEntity;
 import cn.insectmk.gulimall.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+    @Autowired
+    private AttrAttrgroupRelationDao attrAttrgroupRelationDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +32,20 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveAttr(AttrVo attr) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attr, attrEntity);
+        // 保存基本数据
+        this.save(attrEntity);
+        // 保存关联关系
+        AttrAttrgroupRelationEntity relationEntity = new AttrAttrgroupRelationEntity();
+        relationEntity.setAttrGroupId(attr.getAttrGroupId());
+        relationEntity.setAttrId(attrEntity.getAttrId());
+        attrAttrgroupRelationDao.insert(relationEntity);
     }
 
 }
