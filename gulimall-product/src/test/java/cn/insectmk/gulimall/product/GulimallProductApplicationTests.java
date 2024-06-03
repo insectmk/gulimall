06a69beaccd96903d0af0254c0096a88783e0a26
makuning <makun_ing@126.com>
2024-06-03 15:44:52 +0800

    商品服务-引入Redis

--- gulimall-product/src/test/java/cn/insectmk/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/cn/insectmk/gulimall/product/GulimallProductApplicationTests.java
@@ -5,11 +5,24 @@ import cn.insectmk.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.UUID;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
     @Autowired
     private BrandService brandService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void redisTest() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("hello", "world_" + UUID.randomUUID());
+        System.out.println(ops.get("hello"));
+    }
 
     @Test
     void contextLoads() {
